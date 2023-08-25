ef11757326c51ddaec47b34861d294ce5a826959
Caio Ceretta <caioceretta@gmail.com>
2023-08-24 23:33:43 -0300

    Started creating the ignite timer

---
+++ 02-ignite-pomodoro/src/components/Button/index.tsx
@@ -0,0 +1,13 @@
+import { ButtonContainer, ButtonVariants } from './Button.styles'
+
+interface ButtonProps {
+  variant?: ButtonVariants
+}
+
+export function Button({ variant = 'primary' }: ButtonProps) {
+  return (
+    <>
+      <ButtonContainer variant={variant}>Send</ButtonContainer>
+    </>
+  )
+}
